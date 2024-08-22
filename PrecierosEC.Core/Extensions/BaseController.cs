4dcbdc1327e89e201ba249d956ba68049ebef950
Juan Carlos Tacuri <jtacuri@rp3.com.ec>
2024-08-22 10:48:49 -0500

    Commit

--- PrecierosEC.Core/Extensions/BaseController.cs
+++ PrecierosEC.Core/Extensions/BaseController.cs
@@ -9,23 +9,25 @@ namespace PrecierosEC.Core.Extensions
     public class BaseController<T> : Controller
     {
         readonly IServiceErrorLog ErrorLog;
-        public string message = "";
+        protected string message = "";
 
-        public T Model { get; set; }
-        public BaseController(IServiceErrorLog _ErrorLog)
-        {
-            ErrorLog = _ErrorLog;
-        }
-        protected void SaveErrorLog(Exception ex) => this.message = string.Format(MensaggeErrorLog.ErrorGeneral,ErrorLog.SaveErrorlog(ex));
+        protected T Model { get; set; }
+        private Response<T> Data { get; set; }
+        public BaseController(IServiceErrorLog _ErrorLog) => ErrorLog = _ErrorLog;
 
-        protected Response<T> addResponse(T info)
+        protected void SaveErrorLog(Exception ex) => this.message = string.Format(MensaggeErrorLog.ErrorGeneral, ErrorLog.SaveErrorlog(ex));
+
+        private string AddResponse()
         {
-            return new Response<T>
+            this.Data = new Response<T>
             {
                 Mensaje = string.IsNullOrEmpty(this.message) ? "OK" : this.message,
-                Info = info,
+                Info = this.Model,
                 Exito = string.IsNullOrEmpty(this.message)
             };
+
+            return this.message;
+
         }
         //protected Response<T> addResponse<T>()
         //{
@@ -35,16 +37,7 @@ namespace PrecierosEC.Core.Extensions
         //        Exito = string.IsNullOrEmpty(this.message)
         //    };  
         //}
-       // protected IActionResult BadRequestResult()=> BadRequest(addResponse<string>());
-        protected IActionResult HttpResult()
-        {
-            var Response = addResponse(this.Model);
-
-            if (string.IsNullOrEmpty(this.message))
-                return Ok(Response);
-            else
-                return BadRequest(Response);
-
-        }
+        // protected IActionResult BadRequestResult()=> BadRequest(addResponse<string>());
+        protected IActionResult HttpResult() => string.IsNullOrEmpty(AddResponse()) ? Ok(this.Data) : BadRequest(this.Data);
     }
 }
